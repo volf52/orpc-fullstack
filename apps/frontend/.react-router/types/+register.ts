acfc0d029b35acefcc6dca7597b711daf264468f
Arslan <rslnkrmt2552@gmail.com>
2025-05-22 20:21:02 +0500

    add activity feed api

---
+++ apps/frontend/.react-router/types/+register.ts
@@ -0,0 +1,17 @@
+import "react-router";
+
+declare module "react-router" {
+  interface Register {
+    params: Params;
+  }
+
+  interface Future {
+    unstable_middleware: false
+  }
+}
+
+type Params = {
+  "/": {};
+  "/auth/register": {};
+  "/auth/login": {};
+};
